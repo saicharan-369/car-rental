d8c984145ed5d954300c7e19dc996fd94bd7a94f
Allenki Akshay <111362177+allenkiakshay@users.noreply.github.com>
2024-07-11 00:09:29 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 
 function connectDB(){
-mongoose.connect('mongodb+srv://amine:marco123@miniproject.6qhyg5p.mongodb.net/car_rental', {useUnifiedTopology: true, useNewUrlParser:true}) 
+mongoose.connect('mongodb+srv://test:Charan%402004@cluster0.bf0nxpw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {useUnifiedTopology: true, useNewUrlParser:true}) 
 
 const connection = mongoose.connection
 connection.on('connected', ()=>{
